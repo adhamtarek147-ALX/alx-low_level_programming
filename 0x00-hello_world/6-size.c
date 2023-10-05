d140f55eefcf2010bd81ee13a018bdccc72194e1
adhamtarek147-ALX <domatarek2003@gmail.com>
2023-10-05 07:01:26 -0700

    files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: always 0
+ */
+int main(void)
+{
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long: %ld byte(s)\n", sizeof(long long));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
+}
